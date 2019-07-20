dac9b4abb9b32e1248f5ef676edb1e74d32a22f2
Neil T <neiltz711@gmail.com>
2019-07-20 18:05:22 -0400

    Update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
+
+// Redux
+import { connect } from 'react-redux';
 
 // @material-ui
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+// Icons
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import Notifications from '@material-ui/icons/Notifications';
 
-const Navbar = () => {
-  return (
-    <AppBar>
-      <Toolbar>
-        <Button color='inherit' component={Link} to='/'>
-          Home
-        </Button>
-        <div className='nav-container'>
-          <Button color='inherit' component={Link} to='/login'>
-            Login
-          </Button>
-          <Button color='inherit' component={Link} to='/signup'>
-            Signup
-          </Button>
-        </div>
-      </Toolbar>
-    </AppBar>
-  );
+class Navbar extends Component {
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <AppBar>
+        <Toolbar className='nav-container'>
+          {authenticated ? (
+            <Fragment>
+              <Link to='/'>
+                <MyButton tipTitle='Home'>
+                  <HomeIcon />
+                </MyButton>
+              </Link>
+              <MyButton tipTitle='Create a Post'>
+                <AddIcon />
+              </MyButton>
+              <MyButton tipTitle='Notifications'>
+                <Notifications />
+              </MyButton>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color='inherit' component={Link} to='/'>
+                Home
+              </Button>
+              <Button color='inherit' component={Link} to='/login'>
+                Login
+              </Button>
+              <Button color='inherit' component={Link} to='/signup'>
+                Signup
+              </Button>
+            </Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
 };
 
-export default Navbar;
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
